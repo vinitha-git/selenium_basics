16ae6c269a7fd76d376befafbd83c168e42f5057
Vinitha Edwin <vinithaedwin@gmail.com>
2023-11-16 09:08:30 +0530

    Odd Numbers

---
+++ src/main/java/org/seleniumbasics/OddNumbers.java
@@ -0,0 +1,15 @@
+package org.seleniumbasics;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
